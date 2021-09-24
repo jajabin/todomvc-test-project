90ca3c076a82b1eb6841656bb1e23704d080a941
jaja <jkvazi@gmail.com>
2021-09-24 09:37:36 +0200

    DRY & independent test: open_app optimized #2

--- tests/contexts.py
+++ tests/contexts.py
@@ -9,17 +9,13 @@ class AtTodoMvcTest:
     def open_app(self):
         already_tested = browser.matching(have.url_containing(browser.config.base_url))
 
-        browser.open(browser.config.base_url)
+        if already_tested:
+            browser.clear_local_storage()
+
+        browser.open('#/')
         browser.should(have.js_returned(
             True,
             'return Object.keys(require.s.contexts._.defined).length === 39'))
-
-        if already_tested:
-            browser.clear_local_storage()
-            browser.driver.refresh()
-            browser.should(have.js_returned(
-                True,
-                'return Object.keys(require.s.contexts._.defined).length === 39'))
 
         yield
 
