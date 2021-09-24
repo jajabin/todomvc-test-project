c58960ad0ae5ba7252f830d7764a3db87dffe2ec
jaja <jkvazi@gmail.com>
2021-09-24 08:53:10 +0200

    DRY & independent test: open_app changed

--- tests/contexts.py
+++ tests/contexts.py
@@ -8,15 +8,24 @@ class AtTodoMvcTest:
     @pytest.fixture(scope='function', autouse=True)
     def open_app(self):
         if not browser.matching(have.url(browser.config.base_url)):
-            browser.open(browser.config.base_url)
-            browser.should(have.js_returned(
-                True,
-                'return Object.keys(require.s.contexts._.defined).length === 39'))
+            open_base_url()
         else:
-            browser.clear_local_storage()
-            browser.driver.refresh()
-            browser.should(have.js_returned(
-                True,
-                'return Object.keys(require.s.contexts._.defined).length === 39'))
+            open_base_url()
+            clear_local_storage()
 
         yield
+
+
+def open_base_url():
+    browser.open(browser.config.base_url)
+    browser.should(have.js_returned(
+        True,
+        'return Object.keys(require.s.contexts._.defined).length === 39'))
+
+
+def clear_local_storage():
+    browser.clear_local_storage()
+    browser.driver.refresh()
+    browser.should(have.js_returned(
+        True,
+        'return Object.keys(require.s.contexts._.defined).length === 39'))
